a51f82b3270fa9ed710d506fe768f4c1284db736
Santiago Menendez <santiago@Santiagos-iMac.local>
2017-01-28 18:37:03 -0600

    Initial Commit

---
+++ Learning Git Hub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learning Git Hub
+//
+//  Created by Santiago Menendez on 1/28/17.
+//  Copyright © 2017 Santiago Menendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
